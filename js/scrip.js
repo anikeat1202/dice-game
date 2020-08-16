940cabbee0420e96ebcde32987333319b5e17570
Anikeat Sharma <64634601+anikeat1202@users.noreply.github.com>
2020-08-16 22:21:04 +0530

    Add files via upload

--- js/scrip.js
+++ js/scrip.js
@@ -1,37 +1,51 @@
-var n = Math.random()*6+1;
-var q=Math.random()*6+1;
-n=Math.ceil(n);
-q=Math.ceil(q);
-if(n>6)
-{
-    n=10-n;
-}if(q>6)
-{
-    q=10-q;
-}
+document.querySelector("button").addEventListener("click",function(){
 
+var audio = new Audio("./sound/dicesound.mp3");
+audio.play();
 
-var str1="img/dice"+n+".png";
-var str2="img/dice"+q+".png";
+    var n = Math.random()*6+1;
 
+    n=Math.floor(n);
+    if(n>6)
+    {
+        n=10-n;
+    }
+    
+    var str1="img/dice"+n+".png";
+    
+    document.querySelector(".dice1").setAttribute("src",str1);
+    
+    var q=Math.random()*6+1;
+    q=Math.floor(q);
+    if(q>6)
+    {
+        q=10-q;
+    }
+    
+    
+    var str2="img/dice"+q+".png";
+    document.querySelector(".dice2").setAttribute("src",str2);
+    
+    
+    if(n==q)
+    {
+        document.querySelector("h1").innerHTML="Draw!!";
+    }
+    
+    if(n>q)
+    {
+    
+        document.querySelector("h1").innerHTML="Player 1 Wins!!";
+    }
+    
+    else if(n<q){
+    
+        document.querySelector("h1").innerHTML="Player 2 Wins!!";
+    
+    }
+    
+    
+    
 
-if(n==q)
-{
-    document.querySelector("h1").innerHTML="Draw!!";
-}
-
-if(n>q)
-{
-
-    document.querySelector("h1").innerHTML="Player 1 Wins!!";
-}
-
-else if(n<q){
-
-    document.querySelector("h1").innerHTML="Player 2 Wins!!";
-
-}
-
-document.querySelector(".dice1").setAttribute("src",str1);
-document.querySelector(".dice2").setAttribute("src",str2);
+})
 
